e39101e6d91b096950a84c1dbf6f8eed23d802fa
= <=>
2022-07-28 18:16:04 +0530

    added a message for bad tokens

--- middlewares/jwtAuthenticate.js
+++ middlewares/jwtAuthenticate.js
@@ -1,29 +1,41 @@
 const jwt = require("jsonwebtoken");
 
 const jwtAuthenticate = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  let data = { success: 0 };
-  res.locals.jwtAuthentication = data;
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    let data = { success: 0 };
+    res.locals.jwtAuthentication = data;
 
-  if (token == null) {
-    data.status = 401;
-    return next();
-  }
-
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    console.log(err);
-
-    if (err) {
-      data.status =403
-      return next(err);
+    if (token == null) {
+      data.status = 401;
+      return next();
     }
-    data.status =200
-    data.user = user;
-    data.success = 1;
-  });
-  res.locals.jwtAuthentication = data;
-  next();
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      // console.log("error while verifying: ",err)
+      if (err) {
+        data.status = 403;
+        data.err = err;
+        data.success = 0;
+        if (err?.message && (err.message == "invalid signature" || err.message == "jwt malformed" ||err.message == "invalid token")) {
+          data.status = 200;
+          data.success = 0;
+        }  else {
+          return next(err);
+        }
+      } else {
+        data.status = 200;
+        data.user = user;
+        data.success = 1;
+      }
+    });
+    res.locals.jwtAuthentication = data;
+    next();
+  } catch (err) {
+    console.log("catch err",err);
+    next(err);
+  }
 };
 
 module.exports = jwtAuthenticate;
